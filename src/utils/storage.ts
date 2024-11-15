ba7978fc676a58e188770178c91820ae1d558f45
Oscar Vallejo <oscar.vallejo@outlook.com>
2024-11-15 11:23:39 +0100

    token of localStorage is encrypted

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,10 +1,30 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import CryptoJS from "crypto-js";
+import { SECRET_KEY } from "@env";
 
 const TOKEN_KEY = "userToken";
+if (!SECRET_KEY) {
+  throw new Error("SECRET_KEY no está definido en las variables de entorno");
+}
+
+const encrypToken = (token: string): string => {
+  return CryptoJS.AES.encrypt(token, SECRET_KEY).toString();
+};
+
+const desencrypToken = (encryptedToken: string): string | null => {
+  try {
+    const bytes = CryptoJS.AES.decrypt(encryptedToken, SECRET_KEY);
+    return bytes.toString(CryptoJS.enc.Utf8);
+  } catch (error) {
+    console.log("Error al desesncriptar", error);
+    return null;
+  }
+};
 
 export const saveToken = async (token: string): Promise<void> => {
   try {
-    await AsyncStorage.setItem(TOKEN_KEY, token);
+    const encryptedToken = encrypToken(token);
+    await AsyncStorage.setItem(TOKEN_KEY, encryptedToken);
   } catch (error) {
     console.log("Error al guardar el token", error);
   }
@@ -12,8 +32,11 @@ export const saveToken = async (token: string): Promise<void> => {
 
 export const getToken = async (): Promise<string | null> => {
   try {
-    await AsyncStorage.getItem(TOKEN_KEY);
-    return TOKEN_KEY;
+    const encryptedToken = await AsyncStorage.getItem(TOKEN_KEY);
+    if (encryptedToken) {
+      return desencrypToken(encryptedToken);
+    }
+    return null;
   } catch (error) {
     console.log("Error al obtener el token", error);
     return null;
